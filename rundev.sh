c1389085a20164c874fff15645a1d71c7b807cbe
Johan Josefsson <johan.josefsson@rocketmail.com>
2022-01-27 19:58:47 +0000

    deploy on port 80

--- rundev.sh
+++ rundev.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ip=`hostname -I | tr -d ' '`
-cat game2.html | sed 's/:7682/:7683/g' | sed "s/pappaochson\.se/$ip/g" > /website/www/gamedev.html
+cat game2.html | sed 's/\/ws:80/:7683/g' | sed "s/pappaochson\.se/$ip/g" > /website/www/gamedev.html
 cp *.png /website/www/
 echo "http://$ip/gamedev.html"
 cp lws-minimal-ws-server jcserv
