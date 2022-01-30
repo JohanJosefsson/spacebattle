76e9bfb8de893525e26455555bb8008669adbff0
Johan Josefsson <johan.josefsson@rocketmail.com>
2022-01-30 17:07:59 +0100

    spaceship statehandling by jsm

---
+++ jsm.h
@@ -0,0 +1,49 @@
+/* jsm.h */
+
+
+/* Johans hierarchical state machines. Resembles functionality-wise Miro Sameks state machine framework as
+described in his book Practical UML Statecharts in C/C++ */
+
+
+/*
+struct TopologyNode:
+Used to describe the hierearchical state machines topology in the topology_p array in
+state must be a non-negative number. The position in the array corresponds to the state
+number.
+*/
+struct TopologyNode{
+	int state;
+    int superstate;
+	int descend_substate;
+};
+struct MeType;
+typedef int (*handlerfunc_t)(struct MeType * me, int ev);
+typedef void(*onfunc_t)(struct MeType * me);
+
+struct Statefuncs {
+	// Must correspond to the position in the array:
+	const int state_num;
+	const char * name;
+	// Returns 1 if the state is changed in any way, 0 otherwise: TODO, message consumed?
+	handlerfunc_t statehandler;
+	onfunc_t entryhandler;
+	onfunc_t exithandler;
+	onfunc_t inithandler;
+};
+
+struct StateChart {
+	// To be filled out by the user:
+	const struct TopologyNode * topology_p;
+	const struct Statefuncs * state_funcs_p;
+	// Used by the statemachine framework:
+	int current_state;
+	int next_state;
+};
+
+void jsm_init(struct StateChart * sc, struct MeType * user);
+void jsm_dispatch(struct StateChart * sc, struct MeType * user, int evt);
+int jsm_get_state(struct StateChart * sc);
+int jsm_is_in_state(struct StateChart * sc, int state);
+
+#define CHANGE(sc, nxt) \
+do{(sc)->next_state = nxt;}while(0)
--- spaceship.c
+++ spaceship.c
@@ -6,7 +6,7 @@
 #include "spaceship.h"
 #include "spacebattle.h"
 #include "jeq.h"
-
+#include "jsm.h"
 
 
 static void Spaceship_turn_left(struct Spaceship * me);
@@ -15,6 +15,91 @@ static void Spaceship_accelerate(struct Spaceship * me);
 static void Spaceship_decelerate(struct Spaceship * me);
 static void Spaceship_fire(struct Spaceship * me);
 static void Spaceship_move(struct Spaceship * me);
+
+
+
+
+enum State {
+	top_s,
+		flying_s,
+				normal_s,
+					protected_s,
+					unprotected_s,
+				quarterbroken_s,
+				halfbroken_s,
+				threequarterbroken_s,
+		broken_s,
+};
+
+
+static const struct TopologyNode topology[] = {
+	// id, super, descend
+	{top_s, 0, flying_s},
+		{flying_s, top_s, normal_s},
+			{normal_s, flying_s, unprotected_s},
+				{protected_s, normal_s},
+				{unprotected_s, normal_s},
+			{quarterbroken_s, flying_s},
+			{halfbroken_s, flying_s},
+			{threequarterbroken_s, flying_s},
+		{broken_s, top_s}
+};
+
+// Fwd decl.
+//static int running_handler(struct Player * me, int ev);
+// static void running_on_entry(struct Player * me);
+static int top_handler(struct Spaceship * me, int ev);
+static int flying_handler(struct Spaceship * me, int ev);
+static int normal_handler(struct Spaceship * me, int ev);
+static void normal_on_entry(struct Spaceship * me);
+static int protected_handler(struct Spaceship * me, int ev);
+static void protected_on_entry(struct Spaceship * me);
+static int unprotected_handler(struct Spaceship * me, int ev);
+static void unprotected_on_entry(struct Spaceship * me);
+static int quarterbroken_handler(struct Spaceship * me, int ev);
+static void quarterbroken_on_entry(struct Spaceship * me);
+static int halfbroken_handler(struct Spaceship * me, int ev);
+static void halfbroken_on_entry(struct Spaceship * me);
+static int threequarterbroken_handler(struct Spaceship * me, int ev);
+static void threequarterbroken_on_entry(struct Spaceship * me);
+static int broken_handler(struct Spaceship * me, int ev);
+static void broken_on_entry(struct Spaceship * me);
+
+
+
+
+
+
+
+
+
+
+
+
+static struct Statefuncs state_funcs[] = {
+	// #, name, handler, entry, exit, init
+	//	{running, "running", running_handler, running_on_entry, running_on_exit},
+	{top_s, "top"},
+	{flying_s, "flying", flying_handler},
+	{normal_s, "normal", normal_handler, normal_on_entry},
+	{protected_s, "protected", protected_handler, protected_on_entry},
+	{unprotected_s, "unprotected", unprotected_handler, unprotected_on_entry},
+	{quarterbroken_s, "quarterbroken", quarterbroken_handler, quarterbroken_on_entry},
+	{halfbroken_s, "halfbroken", halfbroken_handler, halfbroken_on_entry},
+	{threequarterbroken_s, "threequarterbroken", threequarterbroken_handler, threequarterbroken_on_entry},
+	{broken_s, "broken", broken_handler, broken_on_entry},
+};
+
+
+/*
+static struct StateChart sc = {
+	topology,
+	state_funcs
+};
+*/
+
+
+
 
 
 /* Utilities */
@@ -34,6 +119,11 @@ static int abs(int x)
 enum spaceshipstate { flying, broken };
 enum laserstate { active, inactive };
 struct Spaceship {
+	void * evtData_p;
+    struct StateChart sc;
+
+
+
 	int sub;
 	jpfusr_t usr;
 	float x;
@@ -44,6 +134,7 @@ struct Spaceship {
 	}old;
 	float vel_x;
 	float vel_y;
+	float health;
 	float angle;
 	int w;
 	int h;
@@ -51,6 +142,15 @@ struct Spaceship {
 	enum spaceshipstate state;
 	int spid_spaceship;
 	int spid_broken;
+	int spid_halfbroken;
+	int spid_protected;
+	int spid_quarterbroken;
+	int spid_threequarterbroken;
+	int	cur_spid;
+
+
+
+	int cnt;
 	struct Laser {
 		int sub;
 		float x;
@@ -64,6 +164,181 @@ struct Spaceship {
 };
 
 
+
+
+
+
+///
+static int top_handler(struct Spaceship * me, int ev) { return 0; }
+static int flying_handler(struct Spaceship * me, int ev)
+{
+	//printf("%s %d\n", __FUNCTION__, ev);
+	struct CollisionData * cd = 0;
+	void * data = me->evtData_p;
+	switch (ev) {
+	case EVT_W:
+		Spaceship_accelerate(me);
+		return 1;
+	case EVT_A:
+		Spaceship_turn_left(me);
+		return 1;
+	case EVT_S:
+		Spaceship_decelerate(me);
+		return 1;
+	case EVT_D:
+		Spaceship_turn_right(me);
+		return 1;
+	case EVT_SPACE:
+		Spaceship_fire(me);
+		return 1;
+	case EVT_TICK:
+		Spaceship_tick(me, 0);
+		Spaceship_move(me);
+		return 1;
+	case EVT_TREAD_DENIED:
+		//printf("tread denied\n");
+		;
+		struct TreadRefuse * tr = data;
+		me->x = tr->x;
+		me->y = tr->y;
+		me->vel_x = -1 * sgn(me->vel_x)*0.5;
+		me->vel_y = -1 * sgn(me->vel_y)*0.5;
+		return 1;
+#if 0
+	case COLSIG_LASER:
+		cd = data;
+		//printf("SPACESHIP collision %d\n", cd->id);
+		if (/*cd->col_sig  == COLSIG_LASER &&*/ cd->id != me->laser.sub) {
+			//me->state = broken;
+			me->cnt++;
+			printf("hit! %d\n", me->cnt);
+			CHANGE(&(me->sc), broken_s);
+		}
+		else {
+			printf("self!\n");
+		}
+		return 1;
+#endif
+	}
+	return 0;
+}
+
+static int normal_handler(struct Spaceship * me, int ev) {
+	struct CollisionData * cd = 0;
+	void * data = me->evtData_p;
+	switch (ev) {
+	case COLSIG_LASER:
+		cd = data;
+		if (cd->id != me->laser.sub) {
+			me->cnt++;
+			if (me->cnt > 0)CHANGE(&(me->sc), quarterbroken_s);
+		}
+		return 1;
+	}
+	return 0;
+}
+static void normal_on_entry(struct Spaceship * me)
+{
+	me->cur_spid = me->spid_spaceship;
+	me->cnt = 0;
+	me->health = 1.0;
+}
+static int protected_handler(struct Spaceship * me, int ev) { return 0; }
+static void protected_on_entry(struct Spaceship * me) { return 0; }
+static int unprotected_handler(struct Spaceship * me, int ev) { return 0; }
+static void unprotected_on_entry(struct Spaceship * me) {}
+static int quarterbroken_handler(struct Spaceship * me, int ev) { 
+	struct CollisionData * cd = 0;
+	void * data = me->evtData_p;
+	switch (ev) {
+	case COLSIG_LASER:
+		cd = data;
+		if (cd->id != me->laser.sub) {
+			me->cnt++;
+			if(me->cnt > 7)CHANGE(&(me->sc), halfbroken_s);
+		}
+		return 1;
+	}
+	return 0;
+}
+static void quarterbroken_on_entry(struct Spaceship * me) {
+	me->cur_spid = me->spid_quarterbroken;
+	me->cnt = 0;
+	me->health = 0.75;
+}
+static int halfbroken_handler(struct Spaceship * me, int ev) {
+	struct CollisionData * cd = 0;
+	void * data = me->evtData_p;
+	switch (ev) {
+	case COLSIG_LASER:
+		cd = data;
+		if (cd->id != me->laser.sub) {
+			me->cnt++;
+			if (me->cnt > 12)CHANGE(&(me->sc), threequarterbroken_s);
+		}
+		return 1;
+	}
+	return 0;
+}
+static void halfbroken_on_entry(struct Spaceship * me) {
+	me->cur_spid = me->spid_halfbroken;
+	me->cnt = 0;
+	me->health = 0.5;
+}
+static int threequarterbroken_handler(struct Spaceship * me, int ev) {
+	struct CollisionData * cd = 0;
+	void * data = me->evtData_p;
+	switch (ev) {
+	case COLSIG_LASER:
+		cd = data;
+		if (cd->id != me->laser.sub) {
+			me->cnt++;
+			if (me->cnt > 3)CHANGE(&(me->sc), broken_s);
+		}
+		return 1;
+	}
+	return 0;
+}
+static void threequarterbroken_on_entry(struct Spaceship * me) {
+	me->cur_spid = me->spid_threequarterbroken;
+	me->cnt = 0;
+	me->health = 0.25;
+}
+static int broken_handler(struct Spaceship * me, int ev)
+{
+	//printf("%s %d\n", __FUNCTION__, ev);
+	struct CollisionData * cd = 0;
+	void * data = me->evtData_p;
+	switch (ev) {
+	case EVT_A:
+		Spaceship_turn_left(me);
+		return 1;
+	case EVT_D:
+		Spaceship_turn_right(me);
+		return 1;
+	case EVT_TICK:
+		Spaceship_tick(me, 0);
+		Spaceship_move(me);
+		return 1;
+	}
+	return 0;
+}
+
+static void broken_on_entry(struct Spaceship * me)
+{
+	printf("%s sub=%d\n", __FUNCTION__, me->sub);
+	me->cur_spid = me->spid_broken;
+}
+////
+
+
+
+
+
+
+
+
+
 void Spaceship_print(struct Spaceship * me)
 {
 
@@ -75,6 +350,12 @@ static void on_dispatch(void * receiver, int ev, void * data)
 	struct Spaceship * me = receiver;
 	//printf("on_dispatch e=%lu u=%d sub=%d\n", ev , me->usr, me->sub);
 	struct CollisionData * cd = 0;
+
+	me->evtData_p = data;
+	//if(ev != EVT_TICK)printf("dispatch %d\n", ev);
+	jsm_dispatch(&me->sc, me, ev);
+	me->evtData_p = 0;
+#if 0
 
 	switch (ev) {
 	case EVT_W:
@@ -109,13 +390,16 @@ static void on_dispatch(void * receiver, int ev, void * data)
 		cd = data;
 		//printf("SPACESHIP collision %d\n", cd->id);
 		if (/*cd->col_sig  == COLSIG_LASER &&*/ cd->id != me->laser.sub) {
-			me->state = broken;
+			//me->state = broken;
+			me->cnt++;
+			printf("hit! %d\n", me->cnt);
 		}
 		else {
 			//printf("self!\n");
 		}
 		break;
 	}
+#endif
 }
 
 
@@ -164,6 +448,11 @@ static void on_dispatch_laser(void * receiver, int ev, void * data)
 
 void Spaceship_init(struct Spaceship * me, jpfusr_t usr)
 {
+	struct StateChart sc0 = { 0 };
+	me->sc = sc0;
+	me->sc.topology_p = topology;
+	me->sc.state_funcs_p = state_funcs;
+
 	me->usr = usr;
 	me->x = usr % MAX_X;
 	me->y = usr % MAX_Y;
@@ -176,9 +465,17 @@ void Spaceship_init(struct Spaceship * me, jpfusr_t usr)
 	me->state = flying;
 	me->laser.tmo = 0;
 	me->laser.state = inactive;
+	me->cnt = 0;
+	me->health = 1.0;
 
 	me->spid_spaceship = jpf_create_sprite("spaceship.png");
 	me->spid_broken = jpf_create_sprite("broken.png");
+	me->spid_halfbroken = jpf_create_sprite("halfbroken.png");
+	me->spid_protected = jpf_create_sprite("protected.png");
+	me->spid_quarterbroken = jpf_create_sprite("quarterbroken.png");
+	me->spid_threequarterbroken = jpf_create_sprite("threequarterbroken.png");
+	me->cur_spid = -1;
+
 	me->laser.spid = jpf_create_sprite("laser.png");
 	me->sub = jeq_subscribe(on_dispatch, me);
 	me->laser.sub = jeq_subscribe(on_dispatch_laser, &me->laser);
@@ -222,6 +519,7 @@ static void Spaceship_fire(struct Spaceship * me)
 	me->laser.hidden = 0;
 }
 
+//TODO dead code?
 static void Spaceship_destroy(struct Spaceship * me)
 {
 	me->state = broken;
@@ -261,8 +559,8 @@ static void Spaceship_move(struct Spaceship * me)
 {
 	switch (me->state) {
 	case flying:
-		me->x += me->vel_x;
-		me->y += me->vel_y;
+		me->x += me->vel_x * me->health;
+		me->y += me->vel_y * me->health;
 		Spaceship_prune(me);
 		//    me->x %= me->w;
 		//    me->y %= me->h;
@@ -369,6 +667,9 @@ int Spaceship_draw(struct Spaceship * me, jpfhandle_t h)
 
 	//{ "Id":102, "x":80, "y":120 }
 	if (me->inited) {
+
+		jpf_draw_sprite(h, me->cur_spid, me->x, me->y, me->angle);
+		/*
 		switch (me->state) {
 		case flying:
 			jpf_draw_sprite(h, me->spid_spaceship, me->x, me->y, me->angle);
@@ -377,6 +678,7 @@ int Spaceship_draw(struct Spaceship * me, jpfhandle_t h)
 			jpf_draw_sprite(h, me->spid_broken, me->x, me->y, me->angle);
 			break;
 		}
+		*/
 	}
 	return me->inited;
 }
@@ -396,6 +698,12 @@ struct Spaceship *  new_Spaceship(jpfusr_t usr)
 
 	struct Spaceship * me = malloc(sizeof(struct Spaceship));
 	Spaceship_init(me, usr);
+
+
+	
+	jsm_init(&me->sc, me);
+
+
 
 	return me;
 }
