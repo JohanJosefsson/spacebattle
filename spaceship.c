5d52b0bbd85f5c04989ecd74b54dbe2b371c10b8
Johan Josefsson <johan.josefsson@rocketmail.com>
2022-02-09 19:27:10 +0100

    cleanup

--- spaceship.c
+++ spaceship.c
@@ -68,14 +68,6 @@ static void broken_on_entry(struct Spaceship * me);
 
 
 
-
-
-
-
-
-
-
-
 static struct Statefuncs state_funcs[] = {
 	// #, name, handler, entry, exit, init
 	//	{running, "running", running_handler, running_on_entry, running_on_exit},
@@ -89,16 +81,6 @@ static struct Statefuncs state_funcs[] = {
 	{threequarterbroken_s, "threequarterbroken", threequarterbroken_handler, threequarterbroken_on_entry},
 	{broken_s, "broken", broken_handler, broken_on_entry},
 };
-
-
-/*
-static struct StateChart sc = {
-	topology,
-	state_funcs
-};
-*/
-
-
 
 
 
@@ -116,7 +98,7 @@ static int absv(int x)
 }
 
 
-enum spaceshipstate { flying, broken };
+//enum spaceshipstate { flying, broken };
 enum laserstate { active, inactive };
 struct Spaceship {
 	void * evtData_p;
@@ -139,7 +121,7 @@ struct Spaceship {
 	int w;
 	int h;
 	int inited;
-	enum spaceshipstate state;
+//	enum spaceshipstate state;
 	int spid_spaceship;
 	int spid_broken;
 	int spid_halfbroken;
@@ -214,21 +196,6 @@ static int flying_handler(struct Spaceship * me, int ev)
 		}
 		return 1;
 
-#if 0
-	case COLSIG_LASER:
-		cd = data;
-		//printf("SPACESHIP collision %d\n", cd->id);
-		if (/*cd->col_sig  == COLSIG_LASER &&*/ cd->id != me->laser.sub) {
-			//me->state = broken;
-			me->cnt++;
-			printf("hit! %d\n", me->cnt);
-			CHANGE(&(me->sc), broken_s);
-		}
-		else {
-			printf("self!\n");
-		}
-		return 1;
-#endif
 	}
 	return 0;
 }
@@ -257,8 +224,6 @@ static int protected_handler(struct Spaceship * me, int ev) {
 	switch (ev) {
 	case COLSIG_LASER:
 		return 1;
-//	case COLSIG_WORKSHOP:
-//		return 0;
 	case EVT_BLUESTAR_RST:
 		CHANGE(&(me->sc), unprotected_s);
 		return 1;
@@ -333,7 +298,6 @@ static void threequarterbroken_on_entry(struct Spaceship * me) {
 }
 static int broken_handler(struct Spaceship * me, int ev)
 {
-	//printf("%s %d\n", __FUNCTION__, ev);
 	struct CollisionData * cd = 0;
 	void * data = me->evtData_p;
 	switch (ev) {
@@ -343,7 +307,6 @@ static int broken_handler(struct Spaceship * me, int ev)
 		return 1;
 	case EVT_D:
 		me->angle += 1.0;
-		//Spaceship_turn_right(me);
 		return 1;
 	case EVT_TICK:
 		me->angle -= 0.4;
@@ -361,77 +324,20 @@ static void broken_on_entry(struct Spaceship * me)
 	me->vel_y = 0.0;
 	me->cur_spid = me->spid_broken;
 }
-////
 
 
 
 
 
 
-
-
-
-void Spaceship_print(struct Spaceship * me)
-{
-
-}
 
 
 static void on_dispatch(void * receiver, int ev, void * data)
 {
 	struct Spaceship * me = receiver;
-	//printf("on_dispatch e=%lu u=%d sub=%d\n", ev , me->usr, me->sub);
-	struct CollisionData * cd = 0;
-
 	me->evtData_p = data;
-	//if(ev != EVT_TICK)printf("dispatch %d\n", ev);
 	jsm_dispatch(&me->sc, me, ev);
 	me->evtData_p = 0;
-#if 0
-
-	switch (ev) {
-	case EVT_W:
-		Spaceship_accelerate(me);
-		break;
-	case EVT_A:
-		Spaceship_turn_left(me);
-		break;
-	case EVT_S:
-		Spaceship_decelerate(me);
-		break;
-	case EVT_D:
-		Spaceship_turn_right(me);
-		break;
-	case EVT_SPACE:
-		Spaceship_fire(me);
-		break;
-	case EVT_TICK:
-		Spaceship_tick(me, 0);
-		Spaceship_move(me);
-		break;
-	case EVT_TREAD_DENIED:
-		//printf("tread denied\n");
-                ;
-		struct TreadRefuse * tr = data;
-		me->x = tr->x;
-		me->y = tr->y;
-		me->vel_x = -1 * sgn(me->vel_x)*0.5;
-		me->vel_y = -1 * sgn(me->vel_y)*0.5;
-		break;
-	case COLSIG_LASER:
-		cd = data;
-		//printf("SPACESHIP collision %d\n", cd->id);
-		if (/*cd->col_sig  == COLSIG_LASER &&*/ cd->id != me->laser.sub) {
-			//me->state = broken;
-			me->cnt++;
-			printf("hit! %d\n", me->cnt);
-		}
-		else {
-			//printf("self!\n");
-		}
-		break;
-	}
-#endif
 }
 
 
@@ -450,8 +356,6 @@ static void on_dispatch_laser(void * receiver, int ev, void * data)
 			if (me->x < 0 || me->x > MAX_X || me->y < 0 || me->y > MAX_Y) {
 				me->state = inactive;
 			}
-
-
 			struct TreadData * p = malloc(sizeof(struct TreadData));
 			p->col_sig = me->state == active ? COLSIG_LASER : COLSIG_NOTHING;
 			p->id = me->sub;
@@ -474,12 +378,6 @@ static void on_dispatch_laser(void * receiver, int ev, void * data)
 			me->state = inactive;
 		}
 		break;
-/*
-	case EVT_COLLISION:
-		cd = data;
-		//printf("laser_ collision %d\n", cd->id);
-		break;
-		*/
 	}
 }
 
@@ -495,8 +393,6 @@ void Spaceship_init(struct Spaceship * me, jpfusr_t usr)
 
 	me->usr = usr;
 
-	//me->x = usr % MAX_X;
-	//me->y = usr % MAX_Y;
 	struct SqCoord coord;
 	g_nextSqCoord = Sq_getNextCoord('V', g_nextSqCoord, &coord);
 	me->x = coord.x;
@@ -510,7 +406,6 @@ void Spaceship_init(struct Spaceship * me, jpfusr_t usr)
 	me->h = MAX_Y;
 	me->w = MAX_X;
 	me->inited = 1;
-	me->state = flying;
 	me->laser.tmo = 0;
 	me->laser.state = inactive;
 	me->cnt = 0;
@@ -557,7 +452,6 @@ void Spaceship_deinit(struct Spaceship * me)
 
 static void Spaceship_fire(struct Spaceship * me)
 {
-	if (broken == me->state)return;
 	if (me->laser.tmo)return;
 	me->laser.tmo = 20;
 	me->laser.x = me->x;
@@ -565,12 +459,6 @@ static void Spaceship_fire(struct Spaceship * me)
 	me->laser.angle = me->angle;
 	me->laser.state = active;
 	me->laser.hidden = 0;
-}
-
-//TODO dead code?
-static void Spaceship_destroy(struct Spaceship * me)
-{
-	me->state = broken;
 }
 
 static void Spaceship_turn_left(struct Spaceship * me)
@@ -605,20 +493,11 @@ static void Spaceship_prune(struct Spaceship * me)
 
 static void Spaceship_move(struct Spaceship * me)
 {
-	switch (me->state) {
-	case flying:
 		me->x += me->vel_x * me->health;
 		me->y += me->vel_y * me->health;
 		Spaceship_prune(me);
-		//    me->x %= me->w;
-		//    me->y %= me->h;
 		me->vel_x *= 0.95;
 		me->vel_y *= 0.95;
-		break;
-	case broken:
-		break;
-	}
-	Spaceship_print(me);
 	//if ((abs(me->x - me->old.x) > 0.0001) || (abs(me->y - me->old.y) > 0.0001)) {
 	if (absv(me->x) - absv(me->old.x) || absv(me->y) != absv(me->old.y)) {
 
@@ -637,26 +516,6 @@ static void Spaceship_move(struct Spaceship * me)
 
 void Spaceship_tick(struct Spaceship * me, jpfhandle_t h)//remove?
 {
-	/*
-	if (me->laser.tmo)me->laser.tmo--;
-	if (active == me->laser.state) {
-		me->laser.x += 15 * cos(me->laser.angle * PI / 180.0);
-		me->laser.y += 15 * sin(me->laser.angle * PI / 180.0);
-		if (me->laser.x < 0 || me->laser.x > MAX_X || me->laser.y < 0 || me->laser.y > MAX_Y) {
-			me->laser.state = inactive;
-		}
-
-
-		struct TreadData * p = malloc(sizeof(struct TreadData));
-		p->col_sig = me->laser.state == active ? COLSIG_LASER:COLSIG_NOTHING;
-		p->id = me->laser.sub;
-		p->x = me->laser.x;////
-		p->y = me->laser.y;/////
-		jeq_sendto(EVT_TREAD, p, WORLD);
-
-	}
-	*/
-	if (broken == me->state)return;
 
 	//  KEY_W, KEY_A, KEY_S, KEY_D, KEY_SPACE, NROF_KEYEVT
 	int p = 0;
@@ -717,20 +576,10 @@ int Spaceship_draw(struct Spaceship * me, jpfhandle_t h)
 	if (me->inited) {
 
 		jpf_draw_sprite(h, me->cur_spid, me->x, me->y, me->angle);
-		/*
-		switch (me->state) {
-		case flying:
-			jpf_draw_sprite(h, me->spid_spaceship, me->x, me->y, me->angle);
-			break;
-		case broken:
-			jpf_draw_sprite(h, me->spid_broken, me->x, me->y, me->angle);
-			break;
-		}
-		*/
 	}
 	return me->inited;
 }
-
+/*
 int Spaceship_get_coordinates(struct Spaceship * me, float * x, float * y, float * lasx, float * lasy)
 {
 	*x = me->x;
@@ -738,7 +587,7 @@ int Spaceship_get_coordinates(struct Spaceship * me, float * x, float * y, float
 	*lasx = me->laser.x;
 	*lasy = me->laser.y;
 	return active == me->laser.state;
-}
+}*/
 
 
 struct Spaceship *  new_Spaceship(jpfusr_t usr)
@@ -746,13 +595,7 @@ struct Spaceship *  new_Spaceship(jpfusr_t usr)
 
 	struct Spaceship * me = malloc(sizeof(struct Spaceship));
 	Spaceship_init(me, usr);
-
-
-	
 	jsm_init(&me->sc, me);
-
-
-
 	return me;
 }
 
@@ -763,5 +606,4 @@ void free_Spaceship(struct Spaceship * me)
 	free(me);
 	return;
 }
-
 
