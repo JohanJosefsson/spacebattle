e8521cf79053ad192029654fe56b8a6a138527ff
Johan Josefsson <johan.josefsson@rocketmail.com>
2024-01-01 17:34:18 +0000

    wss

--- rundev.sh
+++ rundev.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 ip=`hostname -I | tr -d ' '`
-cat index.html | sed 's/\/ws:80/:7683/g' | sed "s/pappaochson\.se/$ip/g" > /website/www/tst/gamedev.html
+cat index.html | sed 's/\/ws:80/:7683/g' | sed "s/pappaochson\.se/$ip/g" | sed "s/wss/ws/g" > /website/www/tst/gamedev.html
 cp *.png /website/www/tst
 echo "http://$ip/tst/gamedev.html"
 cp lws-minimal-ws-server jcserv
 cp joystick.js /website/www/tst
 ./jcserv
+
+# Run in browser e.g.:
+# http://192.168.50.159/tst/gamedev.html
+
